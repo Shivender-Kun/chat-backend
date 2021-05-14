76033b29b7c8ac83d8367aa04b6124c66819f097
PervertKun <shivenderkumar761@gmail.com>
2021-05-15 04:33:41 +0530

    default

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 const express = require("express");
 const app = express();
-// const http = require("http");
-// const cors = require("cors");
-// const server = http.createServer(app);
-// const socketio = require("socket.io");
-// const io = socketio(server);
+const http = require("http");
+const cors = require("cors");
+const server = http.createServer(app);
+const socketio = require("socket.io");
+const io = socketio(server);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the server");
 });
 
-// app.use(cors());
-// io.on("connection", (socket) => {
-//   console.log("a user is connected");
+app.use(cors());
+io.on("connection", (socket) => {
+  console.log("a user is connected");
 
-//   socket.on("joined", (name) => {
-//     socket.broadcast.emit("joined", name);
-//   });
+  socket.on("joined", (name) => {
+    socket.broadcast.emit("joined", name);
+  });
 
-//   socket.on("newMessage", (data) => {
-//     socket.broadcast.emit("newMessage", data);
-//   });
+  socket.on("newMessage", (data) => {
+    socket.broadcast.emit("newMessage", data);
+  });
 
-//   socket.on("disconnect", () => {
-//     console.log("user disconnected");
-//   });
-// });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 
-app.listen(process.env.Port || 4000, () => {
+const Port = process.env.Port || 4000;
+
+server.listen(Port, () => {
   console.log(`listening on ${process.env.Port}`);
 });
